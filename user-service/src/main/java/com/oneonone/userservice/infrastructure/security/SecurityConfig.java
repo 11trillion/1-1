413bcf06225bc1b0519753be15e1913f18e0b5c0
Cho2unseo <escces00@gmail.com>
2025-11-28 14:50:51 +0900

    chore: 불필요한 파일 삭제

--- user-service/src/main/java/com/oneonone/userservice/infrastructure/security/SecurityConfig.java
+++ user-service/src/main/java/com/oneonone/userservice/infrastructure/security/SecurityConfig.java
@@ -31,5 +31,4 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-
 }
--- user-service/src/main/java/com/oneonone/userservice/security/JwtTokenProvider.java
+++
@@ -1,8 +0,0 @@
-package com.oneonone.userservice.security;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class JwtTokenProvider {
-
-}

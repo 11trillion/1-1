1cfd87994f70dcda92a85fca5e9c918d89b43577
Eddie <juyoung412@gmail.com>
2025-12-03 14:08:39 +0900

    [feat] Kafka 설정  (#32)
    * betting basic setting
    
    * CRUD 완료
    
    * config 서버 설정 및 오류 수정
    
    * DB,common, swagger 의존성 추가
    
    * 테이블명 변경, pageable 반영
    
    * docker basic setting
    
    * docker basic setting2
    
    * apiResponseEntity 적용 & Transactional 추가
    
    * pageable 기본 값 세팅 변경
    
    * Kafka 설정 완료
    
    * Kafka 설정 2

--- betting-service/src/main/java/com/oneonone/bettingservice/application/service/BettingService.java
+++ betting-service/src/main/java/com/oneonone/bettingservice/application/service/BettingService.java
@@ -1,25 +1,29 @@
 package com.oneonone.bettingservice.application.service;
 
-import com.oneonone.bettingservice.application.dto.BettingRequestDto;
-import com.oneonone.bettingservice.application.dto.BettingResponseDto;
+import com.oneonone.bettingservice.presentation.dto.BettingRequestDto;
+import com.oneonone.bettingservice.presentation.dto.BettingResponseDto;
 import com.oneonone.bettingservice.domain.Betting;
 import com.oneonone.bettingservice.domain.BettingErrorCode;
 import com.oneonone.bettingservice.domain.BettingRepository;
 import com.oneonone.common.exception.BusinessException;
-import com.oneonone.common.util.PageableUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BettingService {
     private final BettingRepository bettingRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     // 베팅 내역 조회
     public Betting betting (UUID bettingId){
@@ -62,6 +66,7 @@ public class BettingService {
     }
 
     // 베팅 생성
+    @Transactional
     public BettingResponseDto createBetting(BettingRequestDto requestDto){
         // 생성
         Betting betting = Betting.createBetting(
@@ -80,6 +85,7 @@ public class BettingService {
     }
 
     // 베팅 수정
+    @Transactional
     public BettingResponseDto updateBetting(BettingRequestDto requestDto){
         Betting betting = betting(requestDto.bettingId());
 
@@ -96,14 +102,40 @@ public class BettingService {
                 requestDto.betResult()
         );
 
+        // Kafka
+//        String topic = "point";
+//        String key = "test";
+//        String message = "경기 결과에 따른 포인트 업데이트";
+//        kafkaTemplate.send(topic, key, message);
+
         return BettingResponseDto.from(betting);
     }
 
     // 베팅 삭제
+    @Transactional
     public void deleteBetting(UUID bettingId, Long userId){
         // 베팅 존재 여부 확인
         Betting betting = betting(bettingId);
 
         betting.softDelete(userId);
     }
+
+    // todo kafka 테스트 - 추후 삭제 예정
+    public String kafkaTest(){
+        // Kafka
+        System.out.println("kafka 테스트 시작");
+        String topic = "point";
+        String key = "test";
+        String message = "경기 결과에 따른 포인트 업데이트";
+        kafkaTemplate.send(topic, key, message);
+
+        return "완료";
+    }
+
+    // todo kafka 테스트 - 추후 삭제 예정
+    @KafkaListener(groupId = "betting", topics = "point")
+    public void cumsumerTest(String message){
+        log.info("Kafka 테스트: " + message);
+    }
+
 }

75729d72cbe5bd8c94818cfb3c7215b84934ae17
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-12-09 11:37:59 +0900

    [Fix] Docker 수정 (#54)
    * refactor: Docker 환경에 맞게 리팩토링
    
    * chore: 오타 수정 및 토픽 추가
    
    * chore: 토픽 이름 수정
    
    * refactor: KafkaConfig 수정
    
    * build: 도커 수정
    
    * build: docker 수정

--- point-service/src/main/java/com/oneonone/pointservice/infrastructure/config/KafkaConfig.java
+++ point-service/src/main/java/com/oneonone/pointservice/infrastructure/config/KafkaConfig.java
@@ -28,7 +28,8 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, BalanceEvent> balanceEventConsumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "point-service");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
@@ -52,7 +53,8 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, BalanceCompensationEvent> balanceCompensationEventProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
@@ -68,7 +70,8 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, CompensationResultEvent> compensationResultEventConsumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "point-service");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);

45d0a8834b768754cbaf1f339da8576b5e4b8217
Cho2unseo <escces00@gmail.com>
2025-12-09 18:25:50 +0900

    feat: gameservice Swagger Authorization 추가

---
+++ game-service/src/main/java/com/oneonone/gameservice/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.oneonone.gameservice.infrastructure.config;
+
+import com.oneonone.common.util.SwaggerHelper;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .addServersItem(new Server().url("/"));
+    }
+
+    @Bean
+    public OpenApiCustomizer customGlobalResponses() {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem ->
+                    pathItem.readOperations().forEach(op -> {
+                        op.getResponses().addApiResponse(
+                                "400",
+                                SwaggerHelper.createBadRequestResponse()
+                        );
+                        op.getResponses().addApiResponse(
+                                "500",
+                                SwaggerHelper.createInternalServerErrorResponse()
+                        );
+                    })
+            );
+        };
+    }
+}

39882f10e0432749801797a85fbf6fc0136a3b0a
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-12-09 14:29:59 +0900

    [Fix] 스웨거 경로 수정 (#56)
    * 오류 수정
    
    * fix: Swagger 경로 수정
    
    ---------
    
    Co-authored-by: jinhyeon <jingoo211@gmail.com>

--- point-service/src/main/java/com/oneonone/pointservice/PointServiceApplication.java
+++ point-service/src/main/java/com/oneonone/pointservice/PointServiceApplication.java
@@ -1,8 +1,15 @@
 package com.oneonone.pointservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Point Service (via Gateway)")
+        }
+)
 @SpringBootApplication(scanBasePackages = {
         "com.oneonone.pointservice",
         "com.oneonone.common"

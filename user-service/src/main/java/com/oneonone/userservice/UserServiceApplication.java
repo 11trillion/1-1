ae49b55334b04a4f0298b15d2103c2ff155c7e27
Cho2unseo <escces00@gmail.com>
2025-12-09 14:25:28 +0900

    fix: Swagger 경로 수정

--- user-service/src/main/java/com/oneonone/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/oneonone/userservice/UserServiceApplication.java
@@ -1,9 +1,16 @@
 package com.oneonone.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "User Service (via Gateway)")
+        }
+)
 @SpringBootApplication(scanBasePackages = {
         "com.oneonone.userservice",
         "com.oneonone.common"

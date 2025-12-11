664541a2400f4cfd00c3f15b61a23f23da068403
Cho2unseo <escces00@gmail.com>
2025-12-11 14:51:54 +0900

    feat: 포인트 저장 아웃박스화

--- user-service/src/main/java/com/oneonone/userservice/infrastructure/kafka/event/BettingEvent.java
+++ user-service/src/main/java/com/oneonone/userservice/infrastructure/kafka/event/BettingEvent.java
@@ -1,6 +1,7 @@
 package com.oneonone.userservice.infrastructure.kafka.event;
 
 public record BettingEvent(
+        String sagaId,
         String eventId,
         Long userId,
         Long amount,
